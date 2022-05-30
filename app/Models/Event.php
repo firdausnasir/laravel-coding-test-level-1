d9ae0eca4eeacc2180e11dc729e557b624c479fa
firdausnasir <firdaus.nasir@involve.asia>
2022-05-30 21:49:53 +0800

    add event model and controller

---
+++ app/Models/Event.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+/**
+ * App\Models\Event
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property string $createdAt
+ * @property string $updatedAt
+ * @method static \Database\Factories\EventFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Event extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        self::creating(function (self $model) {
+            $model->id        = (string) Str::uuid();
+            $model->createdAt = now()->toDateTimeString();
+            $model->updatedAt = now()->toDateTimeString();
+        });
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('events', \App\Http\Controllers\EventController::class);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
